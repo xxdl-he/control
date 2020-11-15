ba82fa510dc7e68e4e392d1fb4e409b58fe12026
xxdlyzbb <3332770186@qq.com>
2020-11-15 09:34:16 +0800

    update control.js.

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
-var inputs = document.all.tags( "input ");
+var inputs = document.getElementsByTagName( "input");
 for (var i=0; i < inputs.length; i++) // 遍历页面上所有的 input
 {
 inputs[i].style.display = "none ";
